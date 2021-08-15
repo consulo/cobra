fe6712afd93ef56f0c424587611b831fa7b1d2ef
VISTALL <vistall.valeriy@gmail.com>
2021-08-15 12:54:18 +0300

    remove unnecessary dependencies
    remove kotlin dependency

---
+++ src/main/java/org/cobraparser/CobraParser.java
@@ -0,0 +1,9 @@
+package org.cobraparser;
+
+public class CobraParser {
+    public static boolean isDebugOn = false;
+
+    public static void setDebug(boolean debug) {
+        isDebugOn = debug;
+    }
+}
